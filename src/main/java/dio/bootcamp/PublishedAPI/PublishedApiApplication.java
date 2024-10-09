ab53959ccba9c9edf4dc026d8355aa0236d23349
JoaoSobral <joaosobralpessoa123@gmail.com>
2024-10-09 14:54:34 -0300

    Starting repo

---
+++ src/main/java/dio/bootcamp/PublishedAPI/PublishedApiApplication.java
@@ -0,0 +1,13 @@
+package dio.bootcamp.PublishedAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/bootcamp/PublishedAPI/PublishedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.bootcamp.PublishedAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
